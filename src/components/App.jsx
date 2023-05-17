ddc70e375a28cf6e05a645338b29508dc9023a25
Piotr Pacuk <105542652+PPacuk@users.noreply.github.com>
2023-05-17 19:54:20 +0200

    added TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import Statistics from './Statistics/Statistics';
 import data from './Statistics/data';
 import FriendList from './FriendList/FriendList';
 import friends from './FriendList/friends.json'
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json'
 
 export const App = () => {
   return (
@@ -17,7 +19,8 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
-      <FriendList friends={friends} />;
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
